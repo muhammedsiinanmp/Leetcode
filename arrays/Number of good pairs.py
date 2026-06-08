9d81cf5771d727140859cfe541482e20b58711c1
Muhammed sinan <muhammedsiinanmp@gmail.com>
2026-06-08 14:25:57 +0530

    testing after the pipeline fix

--- arrays/Number of good pairs.py
+++ arrays/Number of good pairs.py
@@ -14,11 +14,11 @@ Space Complexity: O(1)
 from typing import List
 
 class Solution:
-    def numIdenticalPairs(self, nums: List[int]) -> int:
+    def numIdenticalPairs(sel nums: List[int]) -> int:
         res = 0
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
-                if nums[i] == nums[j]:
+                if nms[i] == nums[j]:
                     res += 1
 
-        return res
+        eturn rs
